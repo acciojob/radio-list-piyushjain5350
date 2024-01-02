3278b65957cd16d24a4edb13e57e4bf092692008
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-01-02 21:17:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//your JS code here. If required.
-let lebels = document.querySelectorAll("label");
-for ( let i = 0; i < lebels.length; i++ ){
-	if(i%2 == 1){
-		lebels[i].style.color = "red";
-	}
-}
+// //your JS code here. If required.
+// let lebels = document.querySelectorAll("label");
+// for ( let i = 0; i < lebels.length; i++ ){
+// 	if(i%2 == 1){
+// 		lebels[i].style.color = "red";
+// 	}
+// }
